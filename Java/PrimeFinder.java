80bbeb7dea1d67c8877af5985a95e774cdb1fe37
Egzander <59097319+Egzander@users.noreply.github.com>
2022-10-29 01:06:43 -0400

    Fixed error

--- Java/PrimeFinder.java
+++ Java/PrimeFinder.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Primes {
+public class PrimeFinder {
   public static void main(String[] args) {
     System.out.println(primeFinder(13));
   }
@@ -12,7 +12,7 @@ public class Primes {
     for(int i = 2; i <= n; i++) {
       boolean prime = true;
 
-      for(int j = 0; j < primes.size(); i++) {
+      for(int j = 0; j < primes.size(); j++) {
         if(i % primes.get(j) == 0) {
           prime = false;
         }
